6da5f69c7053f55cf9706ca8c7570aa04670e97c
okborges <rafaelborges.av@gmail.com>
2023-03-27 19:45:51 -0300

    add js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('input[id="titulo"]').value = 'CERTIFICADO DE BOM GAROTO';
+document.querySelector('input[id="nome"]').value = '';
+document.querySelector('textarea[id="mensagem"]').value = '';
+document.querySelector('input[id="tutor"]').value = 'rafael';
+document.querySelector('input[id="data"]').value = '28/03/2022';
