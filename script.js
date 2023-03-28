c9239491222e317d9f7ee17aff55e3913db324a6
okborges <rafaelborges.av@gmail.com>
2023-03-28 20:09:19 -0300

    scrip de imagem

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ document.querySelector('input[id="nome"]').value = '';
 document.querySelector('textarea[id="mensagem"]').value = '';
 document.querySelector('input[id="tutor"]').value = 'rafael';
 document.querySelector('input[id="data"]').value = '28/03/2022';
+
+function shot(params) {
+  modernScreenshot.domToPng(document.querySelector('main')).then((dataUrl) => {
+    const link = document.createElement('a');
+    link.download = 'screenshot.png';
+    link.href = dataUrl;
+    link.click();
+  });
+}
